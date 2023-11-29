76b2a27830f51272bb4bab4b06f4d004b77079b6
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-29 10:25:39 +0530

    date to unix milliseconds t4

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/api/hello", function (req, res) {
 
 app.get("/api/:date", function (req, res) {
   console.log(req.params);
-  res.json(new Date(req.params.date).getMilliseconds());
+  res.json(new Date(req.params.date).getUTCMilliseconds());
 });
 
 // listen for requests :)
